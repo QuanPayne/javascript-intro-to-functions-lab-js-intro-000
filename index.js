13db0f94079f44f407bedcaaedd9200986cdb78d
Quan Payne <quanxavierp@gmail.com>
2018-07-06 20:34:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function shout(string) {
- return string.toUpperCase()
+ return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout {
-  console.log("HELLO")
+  console.log("HELLO");
 }
 
 function logWhisper {
-  console.log("hello")
+  console.log("hello");
 }
