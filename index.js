17b2a97971b67dba4d451799c4fcd190cda4a6e2
Quan Payne <quanxavierp@gmail.com>
2018-07-06 20:41:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ function shout(string) {
  return shout.toUpperCase();
 }
 
-var uppercase = "HELLO!"
-
-function whisper {
+function whisper(string) {
   return string.toLowerCase();
 }
 
-function logShout {
-  console.log("HELLO");
+function logShout (string) {
+  console.log() string.toUpperCase ;
 }
 
-function logWhisper {
-  console.log("hello");
+function logWhisper (string) {
+ console.log() string.toLowerCase}
+}
+
+function sayHiToGrandma(string) {
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.")) {
+    return "I love you, too."
+  }
+  }
 }
